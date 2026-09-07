b8e09bce99e8cea534fb28edd1e9cc6edc663f8b
TiaraAqilaSaufiana <tiaraaqilasaufiana@gmail.com>
2026-09-07 13:10:55 +0700

    Menambahkan tampilan antarmuka game

--- game.php
+++ game.php
@@ -1,9 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Tebak Angka</title>
+
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f2f2f2;
+            text-align: center;
+            padding-top: 100px;
+        }
+
+        .game {
+            background-color: white;
+            width: 400px;
+            margin: auto;
+            padding: 30px;
+            border-radius: 15px;
+        }
+
+        input {
+            padding: 10px;
+            width: 200px;
+        }
+
+        button {
+            padding: 10px 20px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="game">
+
 <?php
      
      echo "<h2>Game Tebak Angka</h2>";
      echo "Level: 1<br>";
      echo "Masukkan angka dari 1 sampai 50<br>";
-     echo "Kesempatan: 3 kali<br><br>";
+     echo "Kesempatan: 5 kali<br><br>";
      
      $x = rand(1,50);
     // echo "$x<br>";
@@ -27,7 +64,13 @@
                 }
         }
 ?>
+
 <form method="post">
     <input type="number" name="tebak" min="1" max="50" required placeholder="Masukkan angka">
     <button>Tebak</button>
 </form>
+
+</div>
+
+</body>
+</html>
