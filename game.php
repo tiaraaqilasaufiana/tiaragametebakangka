981bd81c7d8458490506f6e56cf2188106f568bc
TiaraAqilaSaufiana <tiaraaqilasaufiana@gmail.com>
2026-09-07 12:25:51 +0700

    Membuat file game tebak angka

---
+++ game.php
@@ -0,0 +1,31 @@
+
+<?php
+     
+     echo "<p>Game Tebak Angka</p>";
+     $x = rand(1,5);
+    // echo "$x<br>";
+
+     if (isset($_POST['tebak']))
+        {
+            $tebakan = $_POST['tebak'];
+            echo "$tebakan<br>";
+
+             if ($_POST['tebak'] == $x)
+                {
+                   echo "Tebakan benar!";
+                } 
+                elseif ($_POST['tebak'] < $x)
+                {
+                   echo "Tebakan hampir benar!";
+                }
+                else
+                {
+                   echo "Tebakan terlalu jauh!";
+                }
+        }
+?>
+<form method="post">
+    <input type="number" name="tebak">
+    <button>Tebak</button>
+</form>
+
